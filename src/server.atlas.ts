25b16745705940fd8f1764aa9078e9cd123c55d6
Ifycode <marychord@gmail.com>
2022-04-06 01:51:18 +0100

    Add mongoDB atlas server connection

---
+++ src/server.atlas.ts
@@ -0,0 +1,16 @@
+import mongooseAtlasConnect from './db.atlas.connect';
+import { app as app } from './app';
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, async () => {
+  try {
+    console.log(`\nServer running at ${process.env.API_HOST_URL}`);
+    await mongooseAtlasConnect();
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+// Chalk import and usage is what causes the import error
+// Also had to use node v16 (was using v14 before - not sure if this is a cause of the issue though...)
