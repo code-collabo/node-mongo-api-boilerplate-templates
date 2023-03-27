4eaf87fdb68bc24f25a44d53fa648ac7f546199d
Ifycode <marychord@gmail.com>
2023-03-27 15:51:57 +0100

    Remove all content

--- src/api/controllers/app.controller.ts
+++
@@ -1,7 +0,0 @@
-import { Request, Response } from 'express';
-
-export const getAppController = async (req: Request, res: Response) => {
-  res.status(200).json({
-    message: 'App works!'
-  });
-}
--- src/api/routes/app.route.ts
+++
@@ -1,8 +0,0 @@
-import express, { IRouter } from 'express';
-import { getAppController } from '../controllers/app.controller';
-
-let router: IRouter = express.Router();
-
-router.get('/', getAppController);
-
-export { router };
