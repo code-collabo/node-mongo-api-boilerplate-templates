83ebd94f13cc23650eea9a0c19b96ccb76e82c62
samuko95 <samuel.c.agba@gmail.com>
2023-04-12 15:35:29 +0100

    restructured app mvc files

--- cjs/src/api/controllers/app.controller.js
+++ cjs/src/api/controllers/app.controller.js
@@ -1,15 +1,13 @@
-const express = require('express');
-const chalk = require('chalk');
 
-let router = express.Router();
+const chalk = require('chalk');
 
 /* eslint-disable no-console */
 
-router.get('/', (req, res) => {//eslint-disable-line no-unused-vars
+const getAppController = (req, res) => {
   res.status(200).json({
     message: 'App works!'
   });
   console.log( chalk.greenBright('\nApp works! \n\nRunning at http://localhost:3000/\n') );
-});
+}
 
-module.exports = router;
+module.exports = {getAppController};
