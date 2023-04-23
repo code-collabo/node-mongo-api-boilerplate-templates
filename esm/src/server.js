92da81c18509032783cd9d15109e178d401cec74
samuko95 <samuel.c.agba@gmail.com>
2023-04-24 00:49:59 +0100

    updated local db files

--- esm/src/server.js
+++ esm/src/server.js
@@ -1,16 +1,8 @@
-import http from 'http';
+import mongooseConnect from './db.connect';
 import { app as app } from './app';
-import chalk from 'chalk';
 
 const port = process.env.PORT || 3000;
-const server = http.createServer(app);
 
-/* eslint-disable no-console */
-
-server.listen(port, () => {
-  try {
-    console.log( chalk.cyanBright(`\nServer running at http://localhost:${port}`) );
-  }catch(err) {
-    console.log(err);
-  }
+app.listen(port, () => {
+  mongooseConnect(port);
 });
