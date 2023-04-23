90088e11367473b3f98f38ed3d952b2fbf3930ee
Ifycode <marychord@gmail.com>
2023-04-23 15:11:51 +0100

    Fix package.json description & ts non-arrow functions

--- ts/src/db.connect.ts
+++ ts/src/db.connect.ts
@@ -6,7 +6,7 @@ import { server, watchEslint } from './lib/script';
 
 dotenv.config();
 
-async function mongooseConnect(port: number | string) {
+const mongooseConnect = async (port: number | string) => {
   try {
     await mongoose.connect(`${process.env.MONGODB_URI}`);
     console.log('\nConnected to local mongoDB');
