7e93d4d561a7c17fbba4d2aaa573fb4fc7c9efc2
samuko95 <samuel.c.agba@gmail.com>
2023-04-24 01:28:52 +0100

    updated local db connect files

--- cjs/src/server.js
+++ cjs/src/server.js
@@ -1,16 +1,8 @@
-const http = require('http');
-const app = require('./app');
-const chalk = require('chalk');
+const mongooseConnect = require('./db.connect');
+const { app } = require('./app');
 
 const port = process.env.PORT || 3000;
-const server = http.createServer(app);
 
-/* eslint-disable no-console */
-
-server.listen(port, () => {
-  try {
-    console.log( chalk.cyanBright(`\nServer running at http://localhost:${port}`) );
-  }catch(err) {
-    console.log(err);
-  }
+app.listen(port, () => {
+  mongooseConnect(port);
 });
