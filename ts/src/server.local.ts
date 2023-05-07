05b459ad2c219b2ca4aa8ff2f3581c347cbcb50f
Ifycode <marychord@gmail.com>
2023-05-07 14:47:04 +0100

    final cleanup - remove local db and server files for esm and ts too

--- ts/src/server.local.ts
+++
@@ -1,8 +0,0 @@
-import mongooseLocalConnect from './db.local.connect';
-import { app as app } from './app';
-
-const port = process.env.PORT_LOCAL || 3000;
-
-app.listen(port, () => {
-  mongooseLocalConnect(port);
-});
