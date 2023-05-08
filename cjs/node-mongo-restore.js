8f092f2d805f46be60299677e03d3b7791fb9136
Ifycode <marychord@gmail.com>
2023-05-08 06:31:42 +0100

    Fix cjs import path, uninstall @code-collabo/node-mongo-scripts v1.0.0

--- cjs/node-mongo-restore.js
+++ cjs/node-mongo-restore.js
@@ -1,3 +1,3 @@
-import { restoreToFirstTimer } from '@code-collabo/node-mongo-scripts';
+import { restoreToFirstTimer } from '@code-collabo/node-mongo-scripts/index';
 
 restoreToFirstTimer('./src', 'cjs');
