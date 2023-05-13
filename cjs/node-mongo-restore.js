8f6b2adca1d6163e058970a7b4e482519d4ea5e8
Ifycode <marychord@gmail.com>
2023-05-13 14:37:12 +0100

    Remove restore file and point dev:change to run node-mongo.js file too

--- cjs/node-mongo-restore.js
+++
@@ -1,3 +0,0 @@
-import { restoreToFirstTimer } from '@code-collabo/node-mongo-scripts/index';
-
-restoreToFirstTimer('./src', 'cjs'); // Do not change the string parameters
--- ts/node-mongo-restore.js
+++
@@ -1,3 +0,0 @@
-import { restoreToFirstTimer } from '@code-collabo/node-mongo-scripts/index';
-
-restoreToFirstTimer('./src', 'ts'); // Do not change the string parameters
