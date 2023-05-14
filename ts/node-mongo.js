cc53f04de53896768e5e2323697cadcb23c2c309
Ifycode <marychord@gmail.com>
2023-05-14 11:41:44 +0100

    Use node-mongo.json to share template name and path with @code-collabo/node-mongo-scripts package

--- ts/node-mongo.js
+++ ts/node-mongo.js
@@ -1,4 +1,4 @@
 import { chooseNodeMongoApiDBServer } from '@code-collabo/node-mongo-scripts/index';
 
-chooseNodeMongoApiDBServer('./src', 'ts'); // Do not change the string parameters
+chooseNodeMongoApiDBServer();
 
